ca52a399313da2086c809142c3762b2c8c434a84
Srinivasulu Puli <srinivas520881@gmail.com>
2023-11-18 22:29:59 +0530

    commit project

---
+++ DemoBitBashApplication/src/main/java/com/example/demo/DemoBitBashApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBitBashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBitBashApplication.class, args);
+	}
+
+}
